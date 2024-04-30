feffd47ff49a05877c396a62aafc399380538aac
ViweTeko <iamviwe.teko@gmail.com>
2024-04-30 23:30:09 +0200

    Binary trees balance fucntion

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -17,3 +17,25 @@ int binary_tree_balance(const binary_tree_t *tree)
 	}
 	return (tot);
 }
+
+/**
+ * tree_height - height of binary tree
+ * @tree: tree
+ * Return: height
+ */
+size_t tree_height(const binary_tree_t *tree)
+{
+	size_t l = 0, r = 0;
+
+	if (tree == NULL)
+		return (0);
+	else
+	{
+		if (tree)
+		{
+			l = tree->left ? 1 + tree_height(tree->left) : 1;
+			r = tree-right ? 1 + tree_height(tree->right) : 1;
+		}
+		return ((l > r) ? l : r);
+	}
+}
